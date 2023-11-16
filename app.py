0dfcda199f54de8bb61818fe9894348d5287c7ae
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2023-11-15 23:49:48 -0500

    changed branch

--- app.py
+++ app.py
@@ -1,166 +1,187 @@
-from flask import Flask, request, render_template, redirect
-from flask import session, make_response
+# """Example flask app demonstrating cookies/sessions/g/auth."""
 
-app = Flask(__name__)
+# import os
+# import functools
 
-# Flask uses a secret key to encrypt cookies used to connect
-# the browser to the session--so if you want to use sessions,
-# you have to have a secret key. If the public learns this
-# value, they can forge session information--so for sites with
-# security concerns, make sure this isn't checked into a
-# public place like GitHub
-app.config["SECRET_KEY"] = "4534gdghjk5d#$RGR^HDG"
+# from flask import Flask, request, render_template, redirect, url_for, make_response
+# from flask import session, flash, g
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_bcrypt import Bcrypt
+# from flask_debugtoolbar import DebugToolbarExtension
+# from flask_login import LoginManager, UserMixin
+# from flask_login import login_user, logout_user, current_user, login_required
 
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/authdemo"
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_ECHO'] = True
+# app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "abc1234")
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-@app.before_request
-def print_cookies():
-    """For every single request that comes in, print out request.cookies (printed to terminal)"""
-    print("*********************")
-    print(request.cookies)
-    print("*********************")
+# db = SQLAlchemy(app)
+# bcrypt = Bcrypt()
 
+# toolbar = DebugToolbarExtension(app)
 
-@app.route("/")
-def index():
-    """Homepage."""
-    return render_template("index.html")
+# login_manager = LoginManager()
 
+# # pass your app into the login_manager instance
+# login_manager.init_app(app)
 
-@app.route("/demo")
-def res_demo():
-    content = "<h1>HELLO!!</h1>"
-    res = make_response(content)
-    res.set_cookie("jolly_rancher_flavor", "grape")
-    return res
+# # You also need to tell flask_login where it should redirect
+# # someone to if they try to access a private route.
+# login_manager.login_view = "users.login"
 
-
-################################################################
-# Routes that demonstrate cookies
-
-
-@app.route("/form-cookie")
-def show_form():
-    """Show form that prompts for favorite color."""
-
-    return render_template("form-cookie.html")
+# # You can also change the default message when someone
+# # gets redirected to the login page. The default message is
+# # "Please log in to access this page."
+# login_manager.login_message = "Please log in!"
 
 
-@app.route("/handle-form-cookie")
-def handle_form():
-    """Return form response; include cookie for browser."""
-
-    fav_color = request.args["fav_color"]
-
-    # Get HTML to send back. Normally, we'd return this, but
-    # we need to do in pieces, so we can add a cookie first
-    html = render_template("response-cookie.html", fav_color=fav_color)
-
-    # In order to set a cookie from Flask, we need to deal
-    # with the response a bit more directly than usual.
-    # First, let's make a response obj from that HTML
-    resp = make_response(html)
-
-    # Let's add a cookie to our response. (There are lots of
-    # other options here--see the Flask docs for how to set
-    # cookie expiration, domain it should apply to, or path)
-    resp.set_cookie("fav_color", fav_color)
-
-    return resp
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(user_id)
 
 
-@app.route("/later-cookie")
-def later():
-    """An example page that can use that cookie."""
+# class User(db.Model, UserMixin):
+#     """Site user."""
 
-    fav_color = request.cookies.get("fav_color", "<unset>")
+#     __tablename__ = "users"
 
-    return render_template("later-cookie.html", fav_color=fav_color)
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     username = db.Column(db.Text, nullable=False, unique=True)
+#     password = db.Column(db.Text, nullable=False)
 
+#     @classmethod
+#     def register(cls, username, password):
+#         """Register a user, hashing their password."""
 
-################################################################
-# Routes that demonstrate sessions
+#         hashed = bcrypt.generate_password_hash(password)
+#         hashed_utf8 = hashed.decode("utf8")
+#         return cls(username=username, password=hashed_utf8)
 
+#     @classmethod
+#     def authenticate(cls, username, password):
+#         """Validate that user exists & password is correct.
 
-@app.route("/form-session")
-def show_sessions_form():
-    """Show form that prompts for nickname and lucky number."""
+#         Return user if valid; else return False.
+#         """
 
-    return render_template("form-session.html")
+#         user = User.query.filter_by(username=username).one()
+#         if user is not None:
+#             if bcrypt.check_password_hash(user.password, password):
+#                 return user
 
-
-@app.route("/handle-form-session")
-def handle_sessions_form():
-    """Return agreeable response and save to session."""
-
-    # Get nickname and lucky number from form and put them
-    # into the session--this will automatically trigger Flask's
-    # session machinery, so it will now send out a cookie with
-    # a session ID. Since we're using the standard
-    # "store session data as a cookie", it will include that
-    session["nickname"] = request.args["nickname"]
-    session["lucky_number"] = int(request.args["lucky_number"])
-
-    # Since we stored this in the session, we don't even need
-    # to pass it to the template directly--jinja templates
-    # automatically have access to session information
-    return render_template("response-session.html")
+#         return False
 
 
-@app.route("/later-session")
-def session_later():
-    """An example page that uses that session info."""
+# ###########################################################################
+# # Useful utilities
 
-    # We could simply get the information from the session
-    # directly in our template (as shown in the template
-    # for handle_session_form), but we'll demonstrate here
-    # that we can also get to the session information in
-    # Flask code
-    nickname = session.get("nickname", "<no nickname>")
+# # @app.before_request
+# # def add_user_to_g():
+# #     """If logged in, add logged-in user obj to g."""
 
-    return render_template("later-session.html", nickname=nickname)
+# #     user_id = session.get("user_id")
+# #     if user_id:
+# #         user = User.query.get(user_id)
+# #         if user is not None:
+# #             g.user = user
+# #         else:
+# #             # couldn't find user -- perhaps user deleted from db?
+# #             raise Exception(f"User #{user_id} missing")
 
+# # def require_login(fn):
+# #     """Decorator for a view function that requires logged-in user.
 
-# **************************
-# SECRET-INVITE DEMO ROUTES:
-# **************************
+# #     If no logged in user, flashes msg & returns to login page.
+# #     """
 
+# #     @functools.wraps(fn)
+# #     def wrapped(*args, **kwargs):
+# #         if hasattr(g, 'user'):
+# #             return fn(*args, **kwargs)
+# #         else:
+# #             flash("Not authorized.")
+# #             return redirect(url_for("login"))
 
-@app.route("/login-form")
-def show_login_form():
-    """Show form that prompts users to enter the secret access code"""
-    return render_template("login-form.html")
+# #     return wrapped
 
-
-@app.route("/login")
-def verify_secret_code():
-    """
-    Checks to see if the entered access code is correct
-
-    - If the code is incorrect, redirect users back to the login form to try again
-
-    - If the code is correct...
-        - set session to indicate that user has access
-        - redirect to the secret invite
-    """
-    SECRET = "chickenz_are_gr8"
-    entered_code = request.args["secret_code"]
-    if entered_code == SECRET:
-        session["entered-pin"] = True
-        return redirect("/secret-invite")
-    else:
-        return redirect("/login-form")
+# ###########################################################################
+# # Routes
 
 
-@app.route("/secret-invite")
-def show_secret_invite():
-    """
-    Check to see if session contains 'entered-pin' (if user entered the correct secret code)
+# @app.route("/")
+# def index():
+#     """Show homepage with counter for # of visits."""
 
-    - If it does, render the invite template
+#     # Get number-of-visits cookies & increment it (start at 1)
 
-    - If session['entered-pin'] is missing or False, redirect user to the form to enter the secret code
-    """
-    if session.get("entered-pin", False):
-        return render_template("invite.html")
-    else:
-        return redirect("/login-form")
+#     if "n_visits" in request.cookies:
+#         n_visits = int(request.cookies['n_visits']) + 1
+#     else:
+#         n_visits = 1
+
+#     # Render homepage, attaching a cookie to the response
+
+#     html = render_template("index.html", n_visits=n_visits)
+#     response = make_response(html)
+#     response.set_cookie("n_visits", str(n_visits))
+#     return response
+
+
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     """Register a user: produce form and handle form submission."""
+
+#     if request.method == "POST":
+#         username = request.form.get("username")
+#         password = request.form.get("password")
+#         user = User.register(username, password)
+#         db.session.add(user)
+#         db.session.commit()
+#         flash("Registered.")
+#         return redirect(url_for("login"))
+
+#     return render_template("register.html")
+
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     """Produce login form or handle login."""
+
+#     if request.method == "POST":
+#         username = request.form.get("username")
+#         password = request.form.get("password")
+
+#         user = User.authenticate(username, password)
+#         if user:
+#             # session['user_id'] = user.id
+#             login_user(user)
+#             flash("Logged in.")
+#             return redirect(url_for("index"))
+
+#         flash("Invalid login.")
+#         # fall through to login.html template
+
+#     return render_template("login.html")
+
+
+# @app.route('/logout', methods=['GET'])
+# def logout():
+#     """Handle logout & redirect to homepage."""
+
+#     logout_user()
+
+#     flash("Logged out.")
+#     return redirect(url_for("index"))
+
+
+# @app.route("/secret")
+# @login_required
+# def secret():
+#     """Secret page for logged-in members only."""
+
+#     print(f"SECURITY: {current_user} is visiting secret area!")
+
+#     return render_template("secret.html")
